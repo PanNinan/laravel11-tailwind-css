17b52b10e532f4049c33e05315fbc81789ff39e1
panninan <panninan@zeaho.com>
2024-05-22 19:40:53 +0800

    add bootstrap

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\IndexController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return response('Hello World');
+    return view('index');
 });
 
 Route::get('/msg', [IndexController::class, "addQueue"]);
